2ce21a6b5e7d582c65e37cef565a11e9f4230a5d
Fadhil Gani <fadhilgani2@gmail.com>
2025-03-04 00:01:00 +0700

    Switch router history mode from WebHistory to WebHashHistory

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import AboutmeSection from '@/components/main-section/content-section/AboutmeSection/index.AboutmeSection.vue';
 import PortfolioSection from '@/components/main-section/content-section/PortfolioSection/index.PortfolioSection.vue';
 import CertifiedSection from '@/components/main-section/content-section/CertifiedSection/index.CertifiedSection.vue';
@@ -13,7 +13,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
